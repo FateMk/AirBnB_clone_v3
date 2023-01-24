1e8d656f3cf36a80f9cb1d47db308629c16543d6
FateMk <markmurimi151@gmail.com>
2023-01-24 23:26:58 +0000

    API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-
-from models import storage
+"""
+Module app
+"""
 from api.v1.views import app_views
+from flasgger import Swagger
+from flask import (Blueprint, Flask, jsonify, make_response)
+from flask_cors import (CORS, cross_origin)
+from models import storage
+from os import getenv
 
-# Flask Applcation variables
+
 app = Flask(__name__)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.register_blueprint(app_views)
+Swagger(app)
 
-# begin flask page rendering
+
+@app.errorhandler(404)
+def not_found(error):
+    """json 404 page"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
-    """
+def teardown(exception):
+    """ closes the session """
     storage.close()
 
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
- #   print(app.url_map)
+#    print(app.url_map)
     app.run(host=host, port=port)
